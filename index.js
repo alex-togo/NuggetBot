f06f139a7840c9586f43bf372f8882b6d305e403
Alex Togo <helloalextogo@gmail.com>
2021-02-14 21:42:51 -0500

    temp key fix

--- index.js
+++ index.js
@@ -13,7 +13,4 @@ client.events = new Discord.Collection();
   require(`./handlers/${handler}`)(client, Discord);
 });
 
-// nugget NzU3MDE3MjkxOTQ1NjcyNzc2.X2aRSw.s2lDFB0fsCCaDlkK0HFavc4orx8
-// test NzgxODk2NzI5MzU1MjIzMDQw.X8EUEw.ZHeRo-ncWveNeSVDOYv4cco3YG0
-
 client.login(process.env.TOKEN);
